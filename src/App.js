b0f3266b4dcfee86367d0daf982444a7e544b600
Halit Enes Kalaycı <59770497+halitkalayci@users.noreply.github.com>
2023-05-03 11:19:48 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// JSX - JavaScript XML
+// HTML & JS
+// class => className
+
+// Frontend Framework => SPA (Single Page Application)
+
+// SSR => Server Side Rendering - Next.js - SEO gelişmiş
+// CSR => Client Side Rendering - Default - SEO zayıf
 
 function App() {
+  // Component
+  // React kodları
+  // let, var, const
+  const name = 10; // Veritabanı bğalantısı - API
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> {name} </h1>
+        <button className="btn btn-primary">Artır</button>
+        <button className="btn btn-danger">Azalt</button>
       </header>
     </div>
   );
